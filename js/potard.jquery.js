330b1f01da8aa3701b46377155ddebe70d86538c
Boutter Loic <loic.boutter@gmail.com>
2013-05-28 19:32:14 +0200

    Remove height and width properties that are calculated with radius. Added css files and renaming

--- js/potard.jquery.js
+++ js/potard.jquery.js
@@ -24,54 +24,60 @@ jQuery(function($) {
 			var $t = $(this);
 			// Get parameters from tag
 			var params = jQuery.extend(jQuery.extend([],defaultParameters),$t.data());
+			// CSS
+			params.width = params.radius*2;
+			params.height = params.radius*2;
 			$t.addClass('potard_value');
+
+			console.log(params);
 
 			// Compute other variables
 			var val = (($(this).val()) ? $(this).val() : (($(this).text()) ? $(this).text() : '0'));
 			var a = (val - params.min) / (params.max - params.min);
 
 			// Wrap element into a div and init canvas
-			$t.wrap('<div class="potard_wrap" />').each(function() {
-				var $div = $t.parent();
-				$circle = $('<canvas height="'+params.height+'px" width="'+params.width+'px"/>');
-				$gauge = $('<canvas height="'+params.height+'px" width="'+params.width+'px"/>');
-				$div.append($circle);
-				$div.append($gauge);
+			$t.wrap('<div class="potard_wrap" />');
+			$t.parents('.potard_wrap').css({'width': params.width+'px', 'height': params.height+'px'});
+			$t.css({'width': '100%', 'height': '100%', 'text-align': 'center', 'line-height': params.height+'px'});
+			var $div = $t.parent();
+			$circle = $('<canvas height="'+params.height+'px" width="'+params.width+'px"/>');
+			$gauge = $('<canvas height="'+params.height+'px" width="'+params.width+'px"/>');
+			$div.append($circle);
+			$div.append($gauge);
 
-				// Draw circle
-				var m = 0;
-				var ctx = $circle[0].getContext('2d');
-				ctx.beginPath();
-				ctx.lineWidth = params.thickness;
-				ctx.strokeStyle = params.lineColor;
-				if(params.shadow) {
-					ctx.shadowOffsetX = params.shadowOffsetX;
-					ctx.shadowOffsetY = params.shadowOffsetY;
-					ctx.shadowBlur = params.shadowBlur;
-					ctx.shadowColor = params.shadowColor;
-					m = (params.shadowOffsetX > params.shadowOffsetY) ? params.shadowOffsetX : params.shadowOffsetY;
-				}
-				ctx.arc(params.width/2, params.height/2, params.radius-m-params.thickness/2, 0, 2*Math.PI);
-				ctx.stroke();
+			// Draw circle
+			var m = 0;
+			var ctx = $circle[0].getContext('2d');
+			ctx.beginPath();
+			ctx.lineWidth = params.thickness;
+			ctx.strokeStyle = params.lineColor;
+			if(params.shadow) {
+				ctx.shadowOffsetX = params.shadowOffsetX;
+				ctx.shadowOffsetY = params.shadowOffsetY;
+				ctx.shadowBlur = params.shadowBlur;
+				ctx.shadowColor = params.shadowColor;
+				m = (params.shadowOffsetX > params.shadowOffsetY) ? params.shadowOffsetX : params.shadowOffsetY;
+			}
+			ctx.arc(params.width/2, params.height/2, params.radius-m-params.thickness/2, 0, 2*Math.PI);
+			ctx.stroke();
 
-				// Draw gauge
-				var ctx = $gauge[0].getContext('2d');
-				compute(params, a, $t, ctx, m);
+			// Draw gauge
+			var ctx = $gauge[0].getContext('2d');
+			compute(params, a, $t, ctx, m);
 
-				// Events
-				$div.click(function(event) {
-					event.preventDefault();
+			// Events
+			$div.click(function(event) {
+				event.preventDefault();
+				compute(params, angle(event, $div, params), $t, ctx, m);
+			});
+			$div.mousedown(function(event) {
+				event.preventDefault();
+				$div.bind('mousemove', (function(event) {
 					compute(params, angle(event, $div, params), $t, ctx, m);
-				});
-				$div.mousedown(function(event) {
-					event.preventDefault();
-					$div.bind('mousemove', (function(event) {
-						compute(params, angle(event, $div, params), $t, ctx, m);
-					}));
-				}).mouseup(function(event) {
-					event.preventDefault();
-					$div.unbind('mousemove');
-				});
+				}));
+			}).mouseup(function(event) {
+				event.preventDefault();
+				$div.unbind('mousemove');
 			});
 		});
 
